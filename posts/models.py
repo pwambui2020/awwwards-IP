2cfa8b05585f383ebbf9ccc62dfd1d5d763a001f
Paulyne <paulynewambui2020@gmail.com>
2020-11-29 04:15:52 +0300

    created my image  model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import datetime as dt 
 
 # Create your models here.
+class Image (models.Model):
+    project_title=models.CharField(max_length=60)
+    project_description = models.TextField()
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    project_image = models.ImageField(default="default.jpeg", upload_to = 'images/')
+    project_link=models.CharField(max_length=60, null=False)
+    
